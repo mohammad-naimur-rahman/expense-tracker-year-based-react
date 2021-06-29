c89f87e4b7d1395756ce93a2e23d5a1a9219acd3
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2021-06-29 11:45:10 +0600

    added

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ExpenseDate.css';
+
+const ExpenseDate = ({ date }) => {
+  const month = date.toLocaleString('en-US', { month: 'long' });
+  const day = date.toLocaleString('en-US', { day: '2-digit' });
+  const year = date.getFullYear();
+  return (
+    <div className=''>
+      <div>{month}</div>
+      <div>{day}</div>
+      <div>{year}</div>
+    </div>
+  );
+};
+
+export default ExpenseDate;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = ({ expense }) => {
-
   const { date, title, amount } = expense;
-
   return (
     <div className='expense-item'>
-      <div>{date.toISOString()}</div>
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
